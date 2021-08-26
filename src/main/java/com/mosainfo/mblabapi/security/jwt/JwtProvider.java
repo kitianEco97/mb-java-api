422220a5ef22095e2ee0e795093d7697b73ec0fc
christian felipe morales sagredo <kitian@MacBook-Air-de-christian.local>
2021-08-26 19:39:39 -0400

    refresh end

--- src/main/java/com/mosainfo/mblabapi/security/jwt/JwtProvider.java
+++ src/main/java/com/mosainfo/mblabapi/security/jwt/JwtProvider.java
@@ -1,14 +1,22 @@
 package com.mosainfo.mblabapi.security.jwt;
 
+import com.mosainfo.mblabapi.security.dto.JwtDto;
 import com.mosainfo.mblabapi.security.entity.UsuarioPrincipal;
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 // GENERA EL TOKEN
 @Component
@@ -24,20 +32,23 @@ public class JwtProvider {
 
     public String generateToken(Authentication authentication) {
         UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
-        return Jwts.builder().setSubject( usuarioPrincipal.getUsername())
+        List<String> roles = usuarioPrincipal.getAuthorities().stream().map( GrantedAuthority::getAuthority ).collect( Collectors.toList());
+        return Jwts.builder()
+                .setSubject( usuarioPrincipal.getUsername())
+                .claim( "roles", roles )
                 .setIssuedAt( new Date() )
                 .setExpiration( new Date(new Date().getTime() + expiration * 1000 ) )
-                .signWith( SignatureAlgorithm.HS512, secret )
+                .signWith( SignatureAlgorithm.HS512, secret.getBytes() )
                 .compact();
     }
 
     public String getNombreUsuarioFromToken(String token) {
-        return Jwts.parser().setSigningKey( secret ).parseClaimsJws( token ).getBody().getSubject();
+        return Jwts.parser().setSigningKey( secret.getBytes() ).parseClaimsJws( token ).getBody().getSubject();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey( secret ).parseClaimsJws( token );
+            Jwts.parser().setSigningKey( secret.getBytes() ).parseClaimsJws( token );
             return true;
         } catch (MalformedJwtException e) {
             logger.error( "Token mal formado" );
@@ -51,5 +62,20 @@ public class JwtProvider {
             logger.error( "Fail en la firma" );
         }
         return false;
+    }
+
+    public String refreshToken(JwtDto jwtDto) throws ParseException {
+        JWT jwt = JWTParser.parse( jwtDto.getToken() );
+        JWTClaimsSet claims = jwt.getJWTClaimsSet();
+        String nombreUsuario = claims.getSubject();
+        List<String> roles = (List<String>) claims.getClaim( "roles" );
+
+        return Jwts.builder()
+                .setSubject( nombreUsuario )
+                .claim( "roles", roles )
+                .setIssuedAt( new Date() )
+                .setExpiration( new Date(new Date().getTime() + expiration ) )
+                .signWith( SignatureAlgorithm.HS512, secret.getBytes() )
+                .compact();
     }
 }
