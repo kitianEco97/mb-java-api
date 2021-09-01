7825e2bdac41dac281cf6396d8496329617147c6
Kitian <you@example.com>
2021-09-01 09:23:10 -0400

    complete system

--- src/main/java/com/mosainfo/mblabapi/emailpassword/controller/EmailController.java
+++ src/main/java/com/mosainfo/mblabapi/emailpassword/controller/EmailController.java
@@ -1,14 +1,22 @@
 package com.mosainfo.mblabapi.emailpassword.controller;
 
 import com.mosainfo.mblabapi.dto.Mensaje;
+import com.mosainfo.mblabapi.emailpassword.dto.ChangePasswordDTO;
 import com.mosainfo.mblabapi.emailpassword.dto.EmailValuesDTO;
 import com.mosainfo.mblabapi.emailpassword.service.EmailService;
+import com.mosainfo.mblabapi.security.entity.Usuario;
+import com.mosainfo.mblabapi.security.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
+import javax.validation.Valid;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -19,18 +27,50 @@ public class EmailController {
     @Autowired
     EmailService emailService;
 
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Value("${spring.mail.username}")
     private String mailFrom;
 
+    private static final String subject = "Cambio de contraseña";
+
     @PostMapping("/send-email")
     public ResponseEntity<?> sendEmailTemplate(@RequestBody EmailValuesDTO dto) {
+        Optional<Usuario> usuarioOpt = usuarioService.getByNombreUsuarioOrEmail(dto.getMailTo());
+        if (!usuarioOpt.isPresent())
+            return  new ResponseEntity<>(new Mensaje("No existe ningún usuario con esas credenciales"), HttpStatus.NOT_FOUND);
+        Usuario usuario = usuarioOpt.get();
         dto.setMailFrom(mailFrom);
-        dto.setSubject("Cambio de contraseña");
-        dto.setUserName("Kitian");
+        dto.setMailTo(usuario.getEmail());
+        dto.setSubject(subject);
+        dto.setUserName(usuario.getNombreUsuario());
         UUID uuid = UUID.randomUUID();
         String tokenPassword = uuid.toString();
         dto.setTokenPassword(tokenPassword);
+        usuario.setTokenPassword(tokenPassword);
+        usuarioService.save(usuario);
         emailService.sendEmail(dto);
         return new ResponseEntity(new Mensaje("Te hemos enviado un correo"), HttpStatus.OK);
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePassword(@Valid @RequestBody ChangePasswordDTO dto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return  new ResponseEntity<>(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
+        if (!dto.getPassword().equals(dto.getConfirmPassword()))
+            return  new ResponseEntity<>(new Mensaje("Las contraseñas no coinciden"), HttpStatus.BAD_REQUEST);
+        Optional<Usuario> usuarioOpt = usuarioService.getByTokenPassword(dto.getTokenPassword());
+        if (!usuarioOpt.isPresent())
+            return  new ResponseEntity<>(new Mensaje("No existe ningún usuario con esas credenciales"), HttpStatus.NOT_FOUND);
+        Usuario usuario = usuarioOpt.get();
+        String newPassword = passwordEncoder.encode(dto.getPassword());
+        usuario.setPassword(newPassword);
+        usuario.setTokenPassword(null);
+        usuarioService.save(usuario);
+        return new ResponseEntity(new Mensaje("Contraseña actualizada"), HttpStatus.OK);
     }
 }
